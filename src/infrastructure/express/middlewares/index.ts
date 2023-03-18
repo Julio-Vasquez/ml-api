3b4a08d27846c46a82aeee336c2915e5237c4b19
Julio-Vasquez <jualvalitube@gmail.com>
2023-03-18 11:57:02 -0500

    feat(middlewares): config cors and morgan

--- src/infrastructure/express/middlewares/index.ts
+++ src/infrastructure/express/middlewares/index.ts
@@ -1,7 +1,11 @@
+import cors from 'cors'
+import morgan from 'morgan'
 import { Express, json } from 'express'
 
 const middleware = (app: Express) => {
     app.use(json())
+    app.use(cors())
+    app.use(morgan('dev'))
 }
 
 export default middleware
