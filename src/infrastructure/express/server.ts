03ed750415ca647b0f89ee78b48830ab38bb1cbd
Julio-Vasquez <jualvalitube@gmail.com>
2023-03-18 17:35:50 -0500

    feat(scripts): config new script start

--- src/infrastructure/express/server.ts
+++ src/infrastructure/express/server.ts
@@ -1,11 +1,8 @@
-import { Express, Response, Request } from 'express'
+import { Express } from 'express'
 
 import { PORT } from './../constants/server.constant'
 
 const server = (app: Express) => {
-    app.get('/', (_req: Request, res: Response) => {
-        res.send('root')
-    })
     app.listen(PORT, () => {
         console.log(`server running in port ${PORT}`)
     })
